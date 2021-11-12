dcaebeaf00a1d88dcbb6818e9485428b813ddf6d
KazukiNakamae <kazukinakamae@gmail.com>
2021-11-12 19:08:52 +0900

    Upload the latest version

---
+++ MaChIAto/MaChIAto.py
@@ -0,0 +1,9 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+
+
+from MaChIAto import main
+
+# This program can only work when you type "python .../MaChIAto.py". Do not import the file.
+if __name__ == '__main__':
+    main()
